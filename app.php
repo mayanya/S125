3acff2c4a3894ceede35c7937798674d1900391f
mayan <mayan@hangsheng.com.cn>
2018-12-06 08:59:21 +0800

    12.6 first test

---
+++ app.php
@@ -0,0 +1,4 @@
+check check check
+you should check carefully before you think that you have completed perfectlly.
+this is what you should know.
+NEXT,i should do something about s32k144 connected with t-box.
